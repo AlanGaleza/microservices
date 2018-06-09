7121e76ad29806594bdeaaecd57bfc7fa7742e85
“AlanGaleza <alano.gal@gmail.com>
2018-06-09 17:20:54 +0200

    security?

---
+++ spring-microservices-oauth-resource/src/main/java/com/cloud/SpringMicroservicesOauthResourceApplication.java
@@ -0,0 +1,60 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.security.Principal;
+
+@EnableResourceServer
+@RestController
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+@SpringBootApplication
+public class SpringMicroservicesOauthResourceApplication {
+
+
+    @RequestMapping("/resource/endpoint")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String endpoint(Principal principal){
+        return "Welcome " + principal.getName()+ ", This message is protected by the resource server.";
+    }
+
+    @Bean
+    public RemoteTokenServices tokenServices(){
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setCheckTokenEndpointUrl("http://localhost:9090/oauth/check_token");
+        tokenServices.setClientId("resource1");
+        tokenServices.setClientSecret("secret");
+        return tokenServices;
+
+    }
+
+    @Bean
+    public TokenStore tokenStore(){
+        return new JdbcTokenStore(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+        dataSource.setUrl("jdbc:hsqldb:sql://localhost/testdb");
+        dataSource.setUsername("SA");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicesOauthResourceApplication.class, args);
+    }
+}
---
+++ spring-microservices-oauth-server/src/main/java/com/cloud/SpringMicroservicesOauthServerApplication.java
@@ -0,0 +1,20 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@RestController
+public class SpringMicroservicesOauthServerApplication {
+
+        public static void main(String[] args) {
+        SpringApplication.run(SpringMicroservicesOauthServerApplication.class, args);
+    }
+}
